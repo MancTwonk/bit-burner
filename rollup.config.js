1e3226090c8bfd07ef4bd7f502bc4d8e7c891bc5
MancTwonk <mark@mandmsoftware.co.uk>
2021-12-31 02:43:23 +0000

    update build process

--- rollup.config.js
+++ rollup.config.js
@@ -1,9 +1,11 @@
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 
-export default {
-  input: "dist/index.js",
+const scriptConfig = (name) => ({
+  input: `dist/${name}.js`,
   output: {
-    file: "bin/build.js",
+    file: `bin/${name}.js`,
   },
   plugins: [nodeResolve()],
-};
+});
+
+export default ["bootstrap", "index"].map(scriptConfig);
